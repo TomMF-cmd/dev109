832cb50b685a9c3b5a9cf1c5fec07751ccec2e0b
TomMF-cmd <tom.maclean@bellevuecollege.edu>
2024-11-08 10:59:36 -0800

    Update multiple-objects.js

--- m07/multiple-objects.js
+++ m07/multiple-objects.js
@@ -28,7 +28,7 @@ var elHotel2 = document.getElementById('hotel2');
 elHotel2.textContent = details2;
 
 var details3 = sunsetHotel.name + ' rooms: ';
-    details3 += parkHotel.checkAvailability();
+    details3 += sunsetHotel.checkAvailability();
 var elHotel3 = document.getElementById('hotel3');
 elHotel3.textContent = details3;
 
